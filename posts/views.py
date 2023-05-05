2865e06de664316c7fe355f7a118cbe5b7a8a761
Gleisson Santos <jsgleisson@gmail.com>
2023-05-04 22:22:39 -0300

    Atualizçaões nos links e fioltros
    Melhorias nos linnks e filttagem de dados.

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
+
 #Utilizando classes onde a vantagem  é que você pode reutilizar o codigo com o sistema de heranças
 
 
@@ -11,14 +13,60 @@ class PostIndex(ListView):
     paginate_by = 6
     context_object_name = 'posts'
 
+    #ordenar os post de forma invertida e filtrando itens publicados apenas
+    def get_queryset(self):
+        qs =  super().get_queryset()
+        qs = qs.order_by('-id').filter(publicado_post=True)
+
+    #obeter numero de comentarios caso ele esteja postado = True
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+
+        )
+        return qs
+    
 
 class PostBusca(PostIndex):
-    pass
+    template_name = 'posts/post_busca.html'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        termo = self.request.GET.get('termo')
+
+        #caso a busca não seja encontrada ele retorna o proprio qs
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) |
+            Q(autor_post__first_name__iexact=termo) |
+            Q(conteudo_post__icontains=termo) |
+            Q(excerto_post__icontains=termo) |
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+
+        return qs
+    
 
 class PostCategoria(PostIndex):
-    pass
+    template_name = 'posts/post_categoria.html'
 
+    def get_queryset(self):
+        qs =  super().get_queryset()
+
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return   qs
+
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+
+        return qs
+    
 
 class PostDetalhes(UpdateView):
     pass

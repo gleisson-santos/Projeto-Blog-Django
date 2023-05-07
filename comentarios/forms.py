4138f3601302085d3916bc1642837cbb1c71ec19
Gleisson Santos <jsgleisson@gmail.com>
2023-05-06 21:32:21 -0300

    Melhorias
    Adicionar as melhorias do campo de comentarios
    melhorado as paginas de talhes dos itens

---
+++ comentarios/forms.py
@@ -0,0 +1,26 @@
+from django.forms import ModelForm
+from .models import Comentario
+
+
+class FormComentario(ModelForm):
+    def clean(self):
+        data = self.cleaned_data
+        nome = data.get('nome_comentario')
+        email = data.get('email_comentario')
+        comentario = data.get('comentario')
+
+    
+    # if not comenatrio:
+    #     self.add_error(
+    #         'comentario',
+    #         'Você não infomrou um comentario!!'
+
+    #     )
+
+
+
+    class Meta:
+        model = Comentario
+        fields = ('nome_comentario', 'email_comentario', 'comentario')
+
+

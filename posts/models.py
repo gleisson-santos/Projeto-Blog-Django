3d5034c46adbbc173d9d453692fe6b9d73f3f4d9
Gleisson Santos <jsgleisson@gmail.com>
2023-05-23 21:54:05 -0300

    Redimensionamento de imagens

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from categorias.models import Categoria
 from django.contrib.auth.models import User
 from django.utils import timezone
+from PIL import Image
+from django.conf import settings
+import os
 
 # Create your models here.
 
@@ -18,12 +21,46 @@ class Post(models.Model):
     def __str__(self):
         return self.titulo_post
 
+    #Reimensionamento de imagens
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        self.resize_image(self.imagem_post.name, 800)
+
+    @staticmethod
+    def resize_image(img_name, new_width):
+        img_path = os.path.join(settings.MEDIA_ROOT, img_name)
+        img = Image.open(img_path)
+        width, height = img.size
+        new_height = round((new_width * height) / width)
+
+        if width <= new_width:
+            img.close()
+            return
+        
+        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        new_img.save(
+            img_path, 
+            optimize=True,
+            quality=60
+        )
+        new_img.close()
+    
+
 class PostImage(models.Model):
     post = models.ForeignKey(Post, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='post_img/%Y/%m/%d', blank=True, null=True)
 
+    #Redimensionamento de imagem
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        Post.resize_image(self.image.name, 800)
+
     def __str__(self):
         return self.post.titulo_post + "Image"
+
+
+
 
 
 

d89a8ee861b1b6890deb828fa8c50509a559514f
Gleisson Santos <jsgleisson@gmail.com>
2023-05-30 21:49:12 -0300

    recaptcha

--- comentarios/forms.py
+++ comentarios/forms.py
@@ -1,13 +1,34 @@
 from django.forms import ModelForm
 from .models import Comentario
+import requests
 
 
 class FormComentario(ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        nome = data.get('nome_comentario')
-        email = data.get('email_comentario')
-        comentario = data.get('comentario')
+
+        raw_data = self.data
+        recaptcha_responde = raw_data.get('g-recaptcha-response')
+
+        recaptcha_request = requests.post(   
+            'https://www.google.com/recaptcha/api/siteverify',
+            data = {
+                'secret': '6LdUwVMmAAAAAE67PNIfK2NYxLqvd1RwxEm8-wBC',
+                'response': recaptcha_responde
+            }
+        )
+
+        recaptcha_result = recaptcha_request.json()
+        if not recaptcha_result.get('success'):
+            self.add_error(
+                'comentario',
+                'Desculpa, ocorreu um erro, tente novamente.'
+            )
+
+
+        cleaned_data = self.cleaned_data
+        nome = cleaned_data.get('nome_comentario')
+        email = cleaned_data.get('email_comentario')
+        comentario = cleaned_data.get('comentario')
 
     
     # if not comenatrio:
@@ -16,8 +37,6 @@ class FormComentario(ModelForm):
     #         'Você não infomrou um comentario!!'
 
     #     )
-
-
 
     class Meta:
         model = Comentario
